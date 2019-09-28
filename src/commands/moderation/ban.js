153679992f9cacb2ed01de0c9644fb09adc3b2d7
JockeRider199 <jockerider199@gmail.com>
2019-09-28 16:16:31 +0200

    Patched ban errors

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,36 +1,35 @@
 const Discord = require('discord.js');
 const moment = require('moment')
 
-exports.run = (client, message, args) => {
-    var reason = args.join(" ").slice();
-    if(!message.member.hasPermission("BAN_MEMBERS")) {
-        var error_permissions = new Discord.RichEmbed()
-            .setDescription("Tu n'as pas la permission de faire ça ! :x: ")
-            .setColor(config.embed.color)
-        message.channel.send(error_permissions)
+exports.run = (client, msg, args) => {
+
+    msg.delete().catch(() => {});
+
+    if(!msg.member.permissions.has("BAN_MEMBERS")) return msg.channel.send(`This command require BAN_MEMBERS permissions`,  {code : true})
+
+    var member = msg.mentions.members.first();
+    var reason = args.slice(2).join(" ");
+
+    if (!member) return msg.channel.send(`You have to mention a member !`, {code : true});
+    if(!reason){
+        reason = "No reason given"
     }
-    if (message.member.hasPermission("BAN_MEMBERS")) {
-        const member = message.mentions.members.first();
-        if (!member) {
-            var error_mention = new Discord.RichEmbed()
-            .setDescription("Mentionne un utilisateur ! :x: ")
-            .setColor(config.embed.color)
-        message.channel.send(error_mention)
-        }
-        member.ban().then(member => {
+
+    member.ban(reason).then((member) => {
+
         var ban_embed = new Discord.RichEmbed()
-            .setDescription("Nouveau BAN ! :hammer: ")
-            .addField("Membre banni", member)
-            .addField("Par :", message.author.username)
-            .addField("Le :", moment().format("LS"))
-            .addField("Raison :", reason)
-            .setColor(config.embed.color)
-       message.channel.send(ban_embed).catch(console.error);
+            .setTitle("Ban Command")
+            .addField("Banned member", `${member} ( ${member.user.tag} )`)
+            .addField("By", msg.author.username)
+            .addField("When", moment().format("L"))
+            .addField("Reason", reason)
+            .setColor(client.config.embed.color)
+       msg.channel.send(ban_embed).catch(console.error);
     })
-    }
-    message.delete();
 }
 
 exports.info = {
-    name: "ban"
+    name: "ban",
+    alias : [],
+    perm : null
 }
