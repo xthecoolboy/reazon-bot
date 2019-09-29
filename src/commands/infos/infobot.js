b3970ef70d6feb27a79063d3fe682092784e99eb
JockeRider199 <jockerider199@gmail.com>
2019-09-29 16:06:50 +0200

    Patched Nivys Pull Request Errors

--- src/commands/infos/infobot.js
+++ src/commands/infos/infobot.js
@@ -1,26 +1,27 @@
 const Discord = require('discord.js');
 
-exports.run = (client, message, args) => {
+exports.run = (client, msg, args) => {
     const { version } = require("discord.js");
-    let stats = require("cpu-stat");
     let os = require('os');
-    let cpu;
-    stats.usagePercent(function(err, percent, seconds) {
-    if (err) {
-        return console.log(err);
-    }
-})
 
-var infobot = new Discord.RichEmbed()
-.setAuthor("Check informations from Reazon#8778", client.user.avatarURL)
-.addField("•📌 Versions :", "NodeJS : " + "`v11.11.0`" + "\n" + "DiscordJS : " + "`" + `v${version}` + "`" + "", true)
-.addField("•📊 Stats :", "`Nombre de serveurs : "+client.guilds.size+"`" + "\n `Nombre d'utilisateurs : " +client.users.size+"`", true)
-.addField("•👥 Developers :", "`Nivys | Kaly#0822`\n`JockeRider199#2627`\n`-ㅡ ㅓ Max ㅏ ㅡ-#7714`", true)
-.addField("•💾 Herberger :", "Plateform : " + "`" +  `${os.platform()}`+ "` \n Architecture : " + "`" + `${os.arch()}` + "` \n Processor : " +  "`" + `${os.cpus().map(i => `${i.model}`)[0]}` + "`")
-.addField("•📰 Heberger config :", "RAM used  dq : " + "`" + `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}` + "MB` | API latency :" + "`" + `${Math.round(client.ping)}` + " ms`")
-.addField("•✅ Online since :", (Math.round(client.uptime / (1000 * 60 * 60))) + ' hours  ' + (Math.round(client.uptime / (1000 * 60)) % 60) + ' minutes ' + (Math.round(client.uptime / 1000) % 60) + ' seconds ')
-.addField("•🌍 Bot make in :", "`France`\n`Belgian`", true)
-.addField("•📡 Héberged in :", "`West Europa`", true)
-.setColor("0xFF0000")
-msg.channel.send(infobot)
+    var infobot = new Discord.RichEmbed()
+        .setAuthor("Check informations from Reazon#8778", client.user.avatarURL)
+        .addField("•📌 Versions :", "NodeJS : " + "`v11.11.0`" + "\n" + "DiscordJS : " + "`" + `v${version}` + "`" + "", true)
+        .addField("•📊 Stats :", "`Nombre de serveurs : "+client.guilds.size+"`" + "\n `Nombre d'utilisateurs : " +client.users.size+"`", true)
+        .addField("•👥 Developers :", "`Nivys | Kaly#0822`\n`JockeRider199#2627`\n`-ㅡ ㅓ Max ㅏ ㅡ-#7714`", true)
+        .addField("•💾 Herberger :", "Plateform : " + "`" +  `${os.platform()}`+ "` \n Architecture : " + "`" + `${os.arch()}` + "` \n Processor : " +  "`" + `${os.cpus().map(i => `${i.model}`)[0]}` + "`")
+        .addField("•📰 Heberger config :", "RAM used  dq : " + "`" + `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}` + "MB` | API latency :" + "`" + `${Math.round(client.ping)}` + " ms`")
+        .addField("•✅ Online since :", (Math.round(client.uptime / (1000 * 60 * 60))) + ' hours  ' + (Math.round(client.uptime / (1000 * 60)) % 60) + ' minutes ' + (Math.round(client.uptime / 1000) % 60) + ' seconds ')
+        .addField("•🌍 Bot make in :", "`France`\n`Belgian`", true)
+        .addField("•📡 Héberged in :", "`West Europa`", true)
+        .setColor("0xFF0000")
+    msg.channel.send(infobot)
+    
+}
+
+exports.info = {
+    name : "botinfo",
+    alias : ["bot-info", "info-bot", "infobot"],
+    perm : null,
+    dir : __dirname
 }
