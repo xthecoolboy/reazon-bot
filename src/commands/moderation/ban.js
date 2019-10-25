d175be2bf4703781834dc816ac6d4f95bd3d9bce
JockeRider199 <jockerider199@gmail.com>
2019-10-25 22:40:42 +0200

    Updated ban command -> Ban by ID

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -6,29 +6,42 @@ exports.run = (client, msg, args) => {
     var member = msg.mentions.members.first();
     var reason = args.slice(2).join(" ");
 
-    if (!member) {
-
-        var error_mention = new Discord.MessageEmbed()
-            .setTitle("⚠️ Error !")
-            .setDescription("You have to mention a member !")
-            .setColor(client.config.embed.color)
-        msg.channel.send(error_mention).catch(console.error);
-    }
     if(!reason){
         reason = "No reason given"
     }
 
-    member.ban({days : 7, reason : reason}).then((member) => {
+    if (!member) {
+        if(isNaN(args[1])){
+            var error_mention = new Discord.MessageEmbed()
+                .setTitle("⚠️ Error !")
+                .setDescription("You have to mention a member !")
+                .setColor(client.config.embed.color)
+            msg.channel.send(error_mention).catch(console.error);
+        }else{
+           msg.guild.members.ban(args[1], {days : 7, reason : reason}).then((user) => {
+                var ban_embed = new Discord.MessageEmbed()
+                    .setTitle("Ban Command")
+                    .addField("👤 Member Banned", `${user} ( ${user.tag} )`)
+                    .addField("🔨 By", msg.author.username)
+                    .addField("🕖 When", moment().format("L"))
+                    .addField("💬 Reason", reason)
+                    .setColor(client.config.embed.color)
+                return msg.channel.send(ban_embed).catch(console.error);
+           });
+        }
+    }else{
+        member.ban({days : 7, reason : reason}).then((member) => {
 
-        var ban_embed = new Discord.MessageEmbed()
-            .setTitle("Ban Command")
-            .addField("👤 Member Banned", `${member} ( ${member.user.tag} )`)
-            .addField("🔨 By", msg.author.username)
-            .addField("🕖 When", moment().format("L"))
-            .addField("💬 Reason", reason)
-            .setColor(client.config.embed.color)
-        msg.channel.send(ban_embed).catch(console.error);
-    })
+            var ban_embed = new Discord.MessageEmbed()
+                .setTitle("Ban Command")
+                .addField("👤 Member Banned", `${member} ( ${member.user.tag} )`)
+                .addField("🔨 By", msg.author.username)
+                .addField("🕖 When", moment().format("L"))
+                .addField("💬 Reason", reason)
+                .setColor(client.config.embed.color)
+            msg.channel.send(ban_embed).catch(console.error);
+        });
+    }
 }
 
 exports.info = {
